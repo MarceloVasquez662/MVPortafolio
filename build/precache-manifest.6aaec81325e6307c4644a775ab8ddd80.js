49dd217bff4f772a045722d6f4eb56a8d15ef9bf
MarceloVasquez662 <marcelovasquez662@gmail.com>
2022-02-19 02:42:59 -0300

    ..

--- build/precache-manifest.6aaec81325e6307c4644a775ab8ddd80.js
+++ build/precache-manifest.6aaec81325e6307c4644a775ab8ddd80.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "91f9c3a0e0c0aa6a990e6d64b9844505",
+    "revision": "e0667eafc39a53fa21e689b2cec8196f",
     "url": "/react_spa/index.html"
   },
   {
-    "revision": "b4efb975fe2d02a7a8c5",
-    "url": "/react_spa/static/css/main.a1e98686.chunk.css"
+    "revision": "01714431e7a9d05a963c",
+    "url": "/react_spa/static/css/main.757f5690.chunk.css"
   },
   {
-    "revision": "0bbb67cb66e3158d0d6b",
-    "url": "/react_spa/static/js/2.df8cf8a8.chunk.js"
+    "revision": "89ddf84083ca306acacc",
+    "url": "/react_spa/static/js/2.6fd9769d.chunk.js"
   },
   {
     "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
-    "url": "/react_spa/static/js/2.df8cf8a8.chunk.js.LICENSE.txt"
+    "url": "/react_spa/static/js/2.6fd9769d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4efb975fe2d02a7a8c5",
-    "url": "/react_spa/static/js/main.e9fac8f0.chunk.js"
+    "revision": "01714431e7a9d05a963c",
+    "url": "/react_spa/static/js/main.b2e8e340.chunk.js"
   },
   {
     "revision": "5d3e7a3f9737ed23208e",
@@ -32,24 +32,32 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/react_spa/static/media/fondoBanner.e6c4e27c.jpg"
   },
   {
-    "revision": "080be0e9b76866fcca6ccd1f6635e640",
-    "url": "/react_spa/static/media/iconProgramacion.080be0e9.svg"
+    "revision": "6a0a2556392fee32b4a9fc88d80e21c5",
+    "url": "/react_spa/static/media/fotoPerfil.6a0a2556.jpg"
   },
   {
     "revision": "ec7f74e08daeaa103d45a8235bbcc442",
     "url": "/react_spa/static/media/logo.ec7f74e0.svg"
   },
   {
+    "revision": "972a70e68cd300d153e1dd5a9f77d58a",
+    "url": "/react_spa/static/media/logoLinea.972a70e6.svg"
+  },
+  {
     "revision": "671fa66c1e7bece0b18102a71692908a",
     "url": "/react_spa/static/media/logoProyectos.671fa66c.svg"
+  },
+  {
+    "revision": "44505fe377fbb7bba800a76fcf08df9a",
+    "url": "/react_spa/static/media/logobook.44505fe3.svg"
   },
   {
     "revision": "7345ae26874f73c49f1a504c35965fc8",
     "url": "/react_spa/static/media/logofolder.7345ae26.svg"
   },
   {
-    "revision": "ac659c971d644766f0106fa149704781",
-    "url": "/react_spa/static/media/logoimage.ac659c97.svg"
+    "revision": "3c6b8959633735cd641e211adf8930c8",
+    "url": "/react_spa/static/media/logoimage.3c6b8959.svg"
   },
   {
     "revision": "3c02366489501b00cfa7209cf47a7179",
@@ -62,5 +70,9 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ff07f21b9e59064d83df290c706216ac",
     "url": "/react_spa/static/media/logopet.ff07f21b.svg"
+  },
+  {
+    "revision": "05b74dd845c3ad87a706a34c48cbb990",
+    "url": "/react_spa/static/media/logoschool.05b74dd8.svg"
   }
 ]);
